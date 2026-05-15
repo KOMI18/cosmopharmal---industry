62560c8493bffc6437067c85b978479ab8229db0
KOMI18 <tehemparfait@gmail.com>
2026-05-15 11:19:46 +0100

    fix: mise a jour de prisma et de son clint

--- src/lib/prisma.tsx
+++ src/lib/prisma.tsx
@@ -1,17 +1,27 @@
 // lib/prisma.ts
 import { PrismaClient } from '@/generated/prisma/client.js';
+import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
-// Déclaration globale pour le type
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const libsql = createClient({
+  url: process.env.DATABASE_URL ?? 'file:./dev.db',
+});
+
+
+const adapter = new PrismaLibSql({
+  url: process.env.DATABASE_URL ?? 'file:./dev.db',
+});
+
 const prisma: PrismaClient = (() => {
   if (process.env.NODE_ENV === 'production') {
-    return new PrismaClient();
+    return new PrismaClient({ adapter });
   } else {
     if (!global.prisma) {
-      global.prisma = new PrismaClient();
+      global.prisma = new PrismaClient({ adapter });
     }
     return global.prisma;
   }
